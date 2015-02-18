e1221da14ae384d642d2b4d055b96413b230bc9c
Daniel P. Clark <6ftdan@gmail.com>
2015-02-17 19:14:05 -0500

    Refactored and tested new internal methods.

--- lib/poly_belongs_to/dup.rb
+++ lib/poly_belongs_to/dup.rb
@@ -4,21 +4,16 @@ module PolyBelongsTo
 
     included do
       def self.pbt_dup_build(item_to_build_on, item_to_duplicate)
-        [:has_one, :has_many].each do |has|
-          reflects = eval(item_to_build_on.class.name).reflect_on_all_associations(has).map(&:name).map(&:to_sym)
-          if reflects.include?(ActiveModel::Naming.singular(item_to_duplicate).to_sym) or
-              reflects.include?(ActiveModel::Naming.plural(item_to_duplicate).to_sym)
+        if PolyBelongsTo::Pbt::IsReflected[item_to_build_on, item_to_duplicate]
+          build_cmd = PolyBelongsTo::Pbt::BuildCmd[item_to_build_on, item_to_duplicate]
+          dup_attrs = PolyBelongsTo::Pbt::AttrSanitizer[item_to_duplicate]
 
-            build_cmd = PolyBelongsTo::Pbt::BuildCmd[has, item_to_duplicate]
-            dup_attrs = PolyBelongsTo::Pbt::AttrSanitizer[item_to_duplicate]
-
-            break build_cmd ? eval("item_to_build_on.#{build_cmd}(#{dup_attrs})") : nil
-          end
+          build_cmd ? eval("item_to_build_on.#{build_cmd}(#{dup_attrs})") : nil
         end
       end
 
       def self.pbt_deep_dup_build(item_to_build_on, item_to_duplicate)
-        pbt_dup_build(item_to_build, item_to_duplicate)
+        built = pbt_dup_build(item_to_build, item_to_duplicate)
         
       end
 
--- lib/poly_belongs_to/poly_belongs_to.rb
+++ lib/poly_belongs_to/poly_belongs_to.rb
@@ -1,24 +1,59 @@
 module PolyBelongsTo
   module Pbt
-    AttrSanitizer = lambda {|pbt_obj|
-      return pbt_obj.dup.attributes.delete_if {|ky,vl|
-        [:created_at, :deleted_at, pbt_obj.pbt_id_sym, pbt_obj.pbt_type_sym].include? ky.to_sym
+    AttrSanitizer = lambda {|obj|
+      obj.dup.attributes.delete_if {|ky,vl|
+        [:created_at, :updated_at, :deleted_at, obj.pbt_id_sym, obj.pbt_type_sym].include? ky.to_sym
       } 
     }
 
-    BuildCmd = lambda {|has, item_to_build|
-      dup_name  = if has == :has_one
-                    ActiveModel::Naming.singular(item_to_build)
-                  else
-                    ActiveModel::Naming.plural( item_to_build )
-                  end
-      return has.eql?(:has_one) ? "build_#{dup_name}" : has.eql?(:has_many) ? "#{dup_name}.build" : nil
+    BuildCmd = lambda {|obj, child|
+      dup_name = "#{CollectionProxy[obj,child]}"
+      IsSingular[obj, child] ? "build_#{dup_name}" : IsPlural[obj, child] ? "#{dup_name}.build" : nil
     }
 
-    SingleOrPlural = lambda {||
+    Reflects = lambda {|obj|
+      [:has_one, :has_many].map { |has|
+        eval(obj.class.name).reflect_on_all_associations(has).map(&:name).map(&:to_sym)
+      }.flatten
+    }
+    
+    IsReflected = lambda {|obj,child|
+      !!SingularOrPlural[obj, child]
     }
 
-    CollectionProxy = lambda {||
+    SingularOrPlural = lambda {|obj, child|
+      reflects = Reflects[obj]
+      if reflects.include?(ActiveModel::Naming.singular(child).to_sym)
+        :singular
+      elsif reflects.include?(ActiveModel::Naming.plural(child).to_sym)
+        :plural
+      else
+        nil
+      end
+    }
+
+    IsSingular = lambda {|obj, child|
+      eval(obj.class.name).reflect_on_all_associations(:has_one).
+        map(&:name).map(&:to_sym).include? ActiveModel::Naming.singular(child).to_sym
+    }
+
+    IsPlural = lambda {|obj,child|
+      eval(obj.class.name).reflect_on_all_associations(:has_many).
+        map(&:name).map(&:to_sym).include? ActiveModel::Naming.plural(child).to_sym
+    }
+
+    CollectionProxy = lambda {|obj, child|
+      reflects = Reflects[obj]
+      proxy = ActiveModel::Naming.singular(child).to_sym
+      if reflects.include? proxy
+        return proxy
+      end
+      proxy = ActiveModel::Naming.plural(child).to_sym
+      if reflects.include? proxy
+        proxy
+      else
+        nil
+      end
     }
   end
 end
