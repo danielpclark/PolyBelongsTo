fb4f79517268cca735525abfe0d9f230cc5cd88f
Daniel P. Clark <6ftdan@gmail.com>
2015-02-19 00:41:54 -0500

    Removed html pages from public folder.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
-# require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require 'rails/test_help'
@@ -21,7 +20,6 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
 
-#reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
 class ActiveSupport::TestCase
