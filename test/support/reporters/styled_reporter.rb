e9edcc08ac6a7cbb49be66298dc16231fb3bf686
Daniel P. Clark <6ftdan@gmail.com>
2015-03-07 11:22:17 -0500

    Updated for both minitest-reporters 0.14.24 and 1.0.11

--- test/support/reporters/styled_reporter.rb
+++ test/support/reporters/styled_reporter.rb
@@ -3,39 +3,36 @@ require 'minitest/reporters'
 class StyledReporter < Minitest::Reporters::SpecReporter
   # minitest-reporters methods for version 0.14.24
   def pass(suite, test, test_runner)
-    common_print(suite, test, test_runner, :green, 'PASS')
+    common_print(suite, test, :green, 'PASS')
   end
 
   def skip(suite, test, test_runner)
-    common_print(suite, test, test_runner, :yellow, 'SKIP')
+    common_print(suite, test, :yellow, 'SKIP')
   end
 
   def failure(suite, test, test_runner)
-    common_print(suite, test, test_runner, :red, 'FAIL')
+    common_print(suite, test, :red, 'FAIL')
     print_exception(test_runner.exception)
   end
 
   def error(suite, test, test_runner)
-    common_print(suite, test, test_runner, :red, 'ERROR')
+    common_print(suite, test, :red, 'ERROR')
     print_exception(test_runner.exception)
   end
 
   # Just in case we load a more recent minitest-reporters v1
   def record(test)
-    super
+    Minitest::Reporters::BaseReporter.instance_method(:record).bind(self).call(test)
     print pad_test(test.name) if test.failure
     print_colored_status(test)
     print(" (%.2fs)" % test.time)
     print " :: #{test.name}" unless test.failure
     puts
-    if !test.skipped? && test.failure
-      print_info(test.failure)
-      puts
-    end
+    print_exception(test.failure) if !test.skipped? && test.failure
   end
 
   private
-  def common_print(suite, test, test_runner, color, message)
+  def common_print(suite, test, color, message)
     print_suite(suite) unless @suites.include?(suite)
     print pad_test(test) if color.eql?(:red)
     print( send(color) { pad_mark(message) } )
