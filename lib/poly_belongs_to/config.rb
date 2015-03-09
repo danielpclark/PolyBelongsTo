422171d9488ac104b242d86852f616bf0be7d2e3
Daniel P. Clark <6ftdan@gmail.com>
2015-03-09 12:31:55 -0400

    internal fetch and delete for default config values

--- lib/poly_belongs_to/config.rb
+++ lib/poly_belongs_to/config.rb
@@ -1,2 +1,9 @@
 class PolyBelongsTo::PbtConfig < OpenStruct
+  def fetch(key, &block)
+    @table.fetch(key.to_sym, &block)
+  end
+
+  def delete(name)
+    @table.delete name.to_sym
+  end
 end
--- test/config_test.rb
+++ test/config_test.rb
@@ -8,6 +8,21 @@ class ConfigTest < ActiveSupport::TestCase
         PolyBelongsTo.config.max_recurse = 42
         PolyBelongsTo.config.max_recurse.must_equal 42
       end
+
+      it "can have internal default methods using singleton method #fetch" do
+        class << PolyBelongsTo.config
+          def example
+            fetch(__method__) { 42 }
+          end
+        end
+        PolyBelongsTo.config.example.must_equal 42
+        PolyBelongsTo.config.example = 53
+        PolyBelongsTo.config.example.must_equal 53
+        PolyBelongsTo.config.delete('example')
+        PolyBelongsTo.config.example.must_equal 42
+        PolyBelongsTo.config.example = 53
+        PolyBelongsTo.config.example.must_equal 53
+      end
     end
   end
 end
